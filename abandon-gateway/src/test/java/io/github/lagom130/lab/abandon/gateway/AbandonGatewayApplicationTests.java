faff37a7749ac90a6a0b877b03a4d962a50ba0c7
lagom <jasonlu1994@hotmail.com>
2023-10-12 23:46:29 +0800

    spring cloud gateway init

---
+++ abandon-gateway/src/test/java/io/github/lagom130/lab/abandon/gateway/AbandonGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lagom130.lab.abandon.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbandonGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
