4e60e4d2f51247565903d9a0be85cf17779d70f0
lagom <jasonlu1994@hotmail.com>
2023-10-21 00:47:44 +0800

    meta service workday controller

--- abandon-meta-service/src/main/java/io/github/lagom130/lab/service/WorkdayService.java
+++ abandon-meta-service/src/main/java/io/github/lagom130/lab/service/WorkdayService.java
@@ -6,9 +6,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class WorkdayService {
@@ -18,20 +17,38 @@ public class WorkdayService {
     public long getWorkdayCountBetween(String startDateStr, String endDateStr) {
         LocalDate startDate = StringUtils.hasLength(startDateStr)?LocalDate.parse(startDateStr):LocalDate.now();
         LocalDate endDate = StringUtils.hasLength(endDateStr)?LocalDate.parse(endDateStr):LocalDate.now();
-        return startDate.datesUntil(endDate)
-                .filter(date -> workDayConfig.getCustomWorkdayDict().getOrDefault(date.toString(),
-                        workDayConfig.getDayOfWeekIsWorkdayDict().getOrDefault(date.getDayOfWeek().getValue(),
-                                false)))
-                .count();
+        // Calculate the number of days between start and end dates
+        long daysBetween = (endDate.isAfter(startDate) ? endDate.toEpochDay() - startDate.toEpochDay() : 0);
+
+        // Calculate the number of workdays between start and end dates
+        long workdayCount = 0;
+        for (int i = 0; i < daysBetween; i++) {
+            LocalDate currentDate = startDate.plusDays(i);
+            if (workDayConfig.getCustomWorkdayDict().getOrDefault(currentDate.toString(),
+                    workDayConfig.getDayOfWeekIsWorkdayDict().getOrDefault(currentDate.getDayOfWeek().getValue(), false))) {
+                workdayCount++;
+            }
+        }
+
+        return workdayCount;
     }
 
     public List<LocalDate> getWorkdayListBetween(String startDateStr, String endDateStr) {
         LocalDate startDate = StringUtils.hasLength(startDateStr)?LocalDate.parse(startDateStr):LocalDate.now();
         LocalDate endDate = StringUtils.hasLength(endDateStr)?LocalDate.parse(endDateStr):LocalDate.now();
-        return startDate.datesUntil(endDate)
-                .filter(date -> workDayConfig.getCustomWorkdayDict().getOrDefault(date.toString(),
-                        workDayConfig.getDayOfWeekIsWorkdayDict().getOrDefault(date.getDayOfWeek().getValue(),
-                                false)))
-                .collect(Collectors.toList());
+        // Calculate the number of days between start and end dates
+        long daysBetween = (endDate.isAfter(startDate) ? endDate.toEpochDay() - startDate.toEpochDay() : 0);
+
+        // Create a list of workdays between start and end dates
+        List<LocalDate> workdayList = new ArrayList<>();
+        for (int i = 0; i < daysBetween; i++) {
+            LocalDate currentDate = startDate.plusDays(i);
+            if (workDayConfig.getCustomWorkdayDict().getOrDefault(currentDate.toString(),
+                    workDayConfig.getDayOfWeekIsWorkdayDict().getOrDefault(currentDate.getDayOfWeek().getValue(), false))) {
+                workdayList.add(currentDate);
+            }
+        }
+
+        return workdayList;
     }
 }
