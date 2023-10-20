6df338142b67c25eae434cce673b9bdaa2524b61
lujc <lujc@inno-view.cn>
2023-10-20 17:58:16 +0800

    meta service init

--- abandon-meta-service/src/main/java/io/github/lagom130/lab/service/WorkdayDataService.java
+++ abandon-meta-service/src/main/java/io/github/lagom130/lab/service/WorkdayDataService.java
@@ -1,9 +1,12 @@
 package io.github.lagom130.lab.service;
 
 import jakarta.websocket.server.PathParam;
+import org.apache.commons.lang.time.DateUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 /**
  * 工作日数据
@@ -17,12 +20,14 @@ public class WorkdayDataService {
 
     }
 
-    public void getByDateBetween(String startDate, String endDate) {
-
+    public void getByDateBetween(String startDateStr, String endDateStr) {
+        LocalDate startDate = LocalDate.parse(startDateStr);
+        LocalDate endDate = LocalDate.parse(endDateStr);
     }
 
-    public void getCountByDateBetween(String startDate, String endDate) {
-
+    public void getCountByDateBetween(String startDateStr, String endDateStr) {
+        LocalDate startDate = LocalDate.parse(startDateStr);
+        LocalDate endDate = LocalDate.parse(endDateStr);
     }
 
 }
