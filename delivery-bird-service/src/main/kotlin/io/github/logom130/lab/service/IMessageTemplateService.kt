be751a1a93a1cd0af72eedc11f33e62cd9d1b6de
lujc <lujc@inno-view.cn>
2023-11-13 11:16:55 +0800

    crud test

---
+++ delivery-bird-service/src/main/kotlin/io/github/logom130/lab/service/IMessageTemplateService.kt
@@ -0,0 +1,45 @@
+package io.github.logom130.lab.service;
+
+import io.github.logom130.lab.entity.MessageTemplate;
+import com.baomidou.mybatisplus.extension.service.IService;
+import io.github.logom130.lab.dto.MessageTemplateDTO
+import io.github.logom130.lab.vo.MessageTemplateVO
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author lagom
+ * @since 2023-11-12
+ */
+interface IMessageTemplateService : IService<MessageTemplate> {
+    /**
+     * 新增
+     * @param messageTemplate
+     * @return
+     */
+    fun addOne(dto : MessageTemplateDTO) : Long
+
+    /**
+     * 删除
+     * @param id
+     * @return
+     */
+    fun deleteOne(id : Long)
+
+    /**
+     * 更新
+     * @param id
+     * @param messageTemplate
+     * @return
+     */
+    fun updateOne(id : Long, dto : MessageTemplateDTO)
+
+    /**
+     * 查询
+     * @param id
+     * @return
+     */
+    fun getOne(id : Long) : MessageTemplateVO
+}
