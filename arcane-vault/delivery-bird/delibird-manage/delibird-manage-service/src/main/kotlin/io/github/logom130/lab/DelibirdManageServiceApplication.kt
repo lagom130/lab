df2a90e1715fcdd7f06113fb60ea0d3ddb4b8c2a
lujc <lujc@inno-view.cn>
2023-11-17 15:07:08 +0800

    delibird init

---
+++ arcane-vault/delivery-bird/delibird-manage/delibird-manage-service/src/main/kotlin/io/github/logom130/lab/DelibirdManageServiceApplication.kt
@@ -0,0 +1,11 @@
+package io.github.logom130.lab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DelibirdManageServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<DelibirdManageServiceApplication>(*args)
+}
---
+++ arcane-vault/delivery-bird/delibird-manage/delibird-manage-service/src/test/kotlin/io/github/logom130/lab/DelibirdManageServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.logom130.lab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DelibirdManageServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
